12c956e399161573f4fa849a49c6887428fcf7b1
Allan Wazacz <cazwazacz@gmail.com>
2017-11-08 14:14:14 +0000

    Adds ability for player 1 to attack player 2

--- app.rb
+++ app.rb
@@ -14,12 +14,18 @@ class Battle < Sinatra::Base
     @player2 = session[:player2]
     @player1hp = 100
     @player2hp = 100
+    @attack_confirmation = session[:attack_confirmation]
     erb(:play)
   end
 
   post '/names' do
     session[:player1] = params[:player1]
     session[:player2] = params[:player2]
+    redirect '/play'
+  end
+
+  post '/attack' do
+    session[:attack_confirmation] = "You've attacked #{session[:player2]}!"
     redirect '/play'
   end
 
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,18 +1,18 @@
 describe 'Battle', :type => :feature do
 
   it 'takes names and shows them on screen' do
-    visit '/'
-    fill_in('player1', with: 'Allan')
-    fill_in('player2', with: 'Dania')
-    click_on('Fight')
+    sign_in_and_play
     expect(page).to have_content "Allan vs Dania"
   end
 
   it 'shows starting HitPoints numbers on screen' do
-    visit '/'
-    fill_in('player1', with: 'Allan')
-    fill_in('player2', with: 'Dania')
-    click_on('Fight')
+    sign_in_and_play
     expect(page).to have_content "100HP ____ 100HP"
+  end
+
+  it 'player 1 attacks player 2 and receives confirmation' do
+    sign_in_and_play
+    click_button('attack player2')
+    expect(page).to have_content "You've attacked Dania!"
   end
 end
